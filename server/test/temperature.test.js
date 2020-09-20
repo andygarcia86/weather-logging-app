d8e979c1e09d685298ad0eb9c81e403da25721fc
Andy Garcia <andygarcia86@gmail.com>
2020-09-20 16:48:37 -0500

    Add unit test

---
+++ server/test/temperature.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const temperatureCtrl = require('../controllers/temperature.controller');
+
+describe('Should return the maximun value of the list', () => {
+    it('should return 5', () => {
+        /*
+        TODO
+        1. Separate logic from Controller to indepent class
+        2. Create method for pass list of values or records
+        3. Crate method for get each statistcs
+        */
+
+        assert.equal(1 + 1, 2);
+    });
+
+});
