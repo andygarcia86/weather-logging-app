333616a01c4a1840c444446780fc9e7dba005aa2
Andy Garcia <andygarcia86@gmail.com>
2020-10-08 15:35:02 -0500

    Add TopHeader component

--- client-vuejs/wheather-loggin/src/App.vue
+++ client-vuejs/wheather-loggin/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <TopHeader/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import TopHeader from './components/TopHeader.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    TopHeader,
     HelloWorld
   }
 }
---
+++ client-vuejs/wheather-loggin/src/components/TopHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <h1>Weather logging</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
