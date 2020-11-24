9df66ac0060872d06b6782ca94171c7fb10e9a8e
Andy Garcia <andygarcia86@gmail.com>
2020-11-23 21:05:00 -0500

    Add router, pages and components

--- client-vuejs/wheather-loggin/src/App.vue
+++ client-vuejs/wheather-loggin/src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <TopHeader/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar/>
+
+    <router-view/>
   </div>
 </template>
 
 <script>
-import TopHeader from './components/TopHeader.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/Navbar.vue';
 
 export default {
   name: 'App',
-  components: {
-    TopHeader,
-    HelloWorld
+  components:{
+    NavBar,
   }
 }
 </script>
@@ -26,6 +24,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ client-vuejs/wheather-loggin/src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+        404: Page not fouond
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage'
+}
+</script>
+
+<style scoped>
+  h1{
+    text-align: center;
+  }
+</style>>
